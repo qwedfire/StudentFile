dac43a5b7d9219c31b1306daceee6bd41e477ccd
qwedfire <qwedfire@gmail.com>
2022-09-05 16:31:01 +0800

    9/5 小修改~

--- src/javaapplication37/Grade.java
+++ src/javaapplication37/Grade.java
@@ -1,68 +1,69 @@
 /**
  * 請寫一個Grade的物件
-屬性：
-科目：Math,Science,English
-成績：0~100
+ * 屬性：
+ * 科目：Math,Science,English
+ * 成績：0~100
  */
 package javaapplication37;
 
 import java.io.Serializable;
 
 /**
- * 
  * @author LAND
  */
-public class Grade implements Serializable{
-    private static final int MAX_SCORE=100;
-    private static final int MAX_SUBJECT=3;
+public class Grade implements Serializable {
+
+    private static final int MAX_SCORE = 100;
+    private static final int MAX_SUBJECT = 3;
     private int mathscore;
     private int sciencescore;
-    private int englishscore;     
+    private int englishscore;
     private int randomgrade;
-    public Grade(){
+
+    public Grade() {
         setscore();
     }
-    public void setscore(){
-        int randomgrade=(int)(Math.random()*(MAX_SUBJECT+1));
-        this.randomgrade=randomgrade;
+
+    public void setscore() {
+        int randomgrade = (int) (Math.random() * (MAX_SUBJECT + 1));
+        this.randomgrade = randomgrade;
         int score;
-        for(int i=0;i<randomgrade;i++){
-            score=(int)(Math.random()*(MAX_SCORE+1));
-            if(i==0){
-                this.mathscore=score;
+        for (int i = 0; i < randomgrade; i++) {
+            score = (int) (Math.random() * (MAX_SCORE + 1));
+            if (i == 0) {
+                this.mathscore = score;
+            } else if (i == 1) {
+                this.sciencescore = score;
+            } else {
+                this.englishscore = score;
             }
-            else if(i==1){
-                this.sciencescore=score;
-            }
-            else {
-                this.englishscore=score;
-            }
-        }        
+        }
     }
-    public int getmathscore(){
+
+    public int getmathscore() {
         return mathscore;
     }
-    public int getsciencescore(){
+
+    public int getsciencescore() {
         return sciencescore;
     }
-    public int getenglishscore(){
+
+    public int getenglishscore() {
         return englishscore;
     }
+
     @Override
-    public String toString(){
-        if(randomgrade==3){
-        return "數學成績:"+this.mathscore+
-                "自然成績:"+this.sciencescore+
-                "英文成績:"+this.englishscore;
-        }
-        else if(randomgrade==2){
-            return "數學成績:"+this.mathscore+
-                "自然成績:"+this.sciencescore;
-        }
-        else if(randomgrade==1){
-            return "數學成績:"+this.mathscore;              
-        }
-        else {
+    public String toString() {
+        if (randomgrade == 3) {
+            return "數學成績:" + this.mathscore +
+                    "自然成績:" + this.sciencescore +
+                    "英文成績:" + this.englishscore;
+        } else if (randomgrade == 2) {
+            return "數學成績:" + this.mathscore +
+                    "自然成績:" + this.sciencescore;
+        } else if (randomgrade == 1) {
+            return "數學成績:" + this.mathscore;
+        } else {
             return "";
         }
     }
--- src/javaapplication37/Student.java
+++ src/javaapplication37/Student.java
@@ -1,120 +1,136 @@
 /**
-請寫一個Student的物件
-屬性：
-學生編號：自動產生的流水號
-學生姓名：長度介於4~8個英文字
-email：長度介於4~12個英文字 + [@gmail.com,@yahoo.com.tw,@hotmail.com]隨機取一個
-手機號碼：09開頭+8個隨機數字
-成績單：ArrayList<Grade>
+ * 請寫一個Student的物件
+ * 屬性：
+ * 學生編號：自動產生的流水號
+ * 學生姓名：長度介於4~8個英文字
+ * email：長度介於4~12個英文字 + [@gmail.com,@yahoo.com.tw,@hotmail.com]隨機取一個
+ * 手機號碼：09開頭+8個隨機數字
+ * 成績單：ArrayList<Grade>
  */
 package javaapplication37;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+
 /**
- * 
  * @author LAND
  */
-public class Student implements Serializable {   
-    public enum Emailtype{
+public class Student implements Serializable {
+
+    public enum Emailtype {
         gmail("@gmail.com"),
         yahoo("@yahoo.com.tw"),
         hotmail("@hotmail.com");
         private String value;
-        Emailtype(String value){
-            this.value=value;
-        }        
-        public String getvalue(){
+
+        Emailtype(String value) {
+            this.value = value;
+        }
+
+        public String getvalue() {
             return value;
         }
-        public static Emailtype getemailtype(int index){
-            Emailtype temp[]=Emailtype.values();
-            return temp[index-1];
+
+        public static Emailtype getemailtype(int index) {
+            Emailtype temp[] = Emailtype.values();
+            return temp[index - 1];
         }
-    }    
-    private static  int SERIAL_NUMBER=1;
-    private static final int MAXNAME_NUMBER=8; //名子最多8個字
-    private static final int MINNAME_NUMBER=4; //名子最少4個字
-    private static final int MINMAIL_NUMBER=4;  //信箱最少4個字
-    private static final int MAXMAIL_NUMBER=12;//信箱最多12個字
-    private static final int MAXCHAR_NUMBER=57;//char數字最大號碼(9)
-    private static final int MINCHAR_NUMBER=48;//char數字最小號碼(0)
-    private static final int MAXENGLISH_NUMBER=122;//char英文最大號碼(z)
-    private static final int MINENGLISH_NUMBER=97;//char英文最小號碼(a)
+    }
+
+    private static int SERIAL_NUMBER = 1;
+    private static final int MAXNAME_NUMBER = 8; //名子最多8個字
+    private static final int MINNAME_NUMBER = 4; //名子最少4個字
+    private static final int MINMAIL_NUMBER = 4;  //信箱最少4個字
+    private static final int MAXMAIL_NUMBER = 12;//信箱最多12個字
+    private static final int MAXCHAR_NUMBER = 57;//char數字最大號碼(9)
+    private static final int MINCHAR_NUMBER = 48;//char數字最小號碼(0)
+    private static final int MAXENGLISH_NUMBER = 122;//char英文最大號碼(z)
+    private static final int MINENGLISH_NUMBER = 97;//char英文最小號碼(a)
     private int id;
     private String name;
     private String email;
     private String cellphone;
     private ArrayList<Grade> studentgrade;
-    public Student(){
-        this.id=SERIAL_NUMBER;
+
+    public Student() {
+        this.id = SERIAL_NUMBER;
         SERIAL_NUMBER++;
         setname();
         setemail();
-        setcellphone();   
-        setgrade();        
-    }    
-    public void setname(){        
-        String randomname="";
-        for(int i=0;i<(int)(Math.random()*(MAXNAME_NUMBER-MINNAME_NUMBER+1)+MINNAME_NUMBER);i++){
-            randomname+=(char)(Math.random()*(MAXENGLISH_NUMBER-MINENGLISH_NUMBER+1)+MINENGLISH_NUMBER);
-        }
-        this.name=randomname;
+        setcellphone();
+        setgrade();
     }
-    public void setemail(){
-        String randomemail="";
-        Emailtype mail=Emailtype.getemailtype((int)(Math.random()*3+1));
-        for(int i=0;i<(int)(Math.random()*(MAXMAIL_NUMBER-MINMAIL_NUMBER+1)+MINMAIL_NUMBER);i++){
-            randomemail+=(char)(Math.random()*(MAXENGLISH_NUMBER-MINENGLISH_NUMBER+1)+MINENGLISH_NUMBER);
-        }        
-        switch(mail){
+
+    public void setname() {
+        String randomname = "";
+        for (int i = 0; i < (int) (Math.random() * (MAXNAME_NUMBER - MINNAME_NUMBER + 1) + MINNAME_NUMBER); i++) {
+            randomname += (char) (Math.random() * (MAXENGLISH_NUMBER - MINENGLISH_NUMBER + 1) + MINENGLISH_NUMBER);
+        }
+        this.name = randomname;
+    }
+
+    public void setemail() {
+        String randomemail = "";
+        Emailtype mail = Emailtype.getemailtype((int) (Math.random() * 3 + 1));
+        for (int i = 0; i < (int) (Math.random() * (MAXMAIL_NUMBER - MINMAIL_NUMBER + 1) + MINMAIL_NUMBER); i++) {
+            randomemail += (char) (Math.random() * (MAXENGLISH_NUMBER - MINENGLISH_NUMBER + 1) + MINENGLISH_NUMBER);
+        }
+        switch (mail) {
             case gmail:
-                randomemail+=mail.getvalue();
+                randomemail += mail.getvalue();
                 break;
             case yahoo:
-                randomemail+=mail.getvalue();
+                randomemail += mail.getvalue();
                 break;
-            case hotmail:   
+            case hotmail:
                 defalut:
-                randomemail+=mail.getvalue();
+                randomemail += mail.getvalue();
                 break;
         }
-        this.email=randomemail;
+        this.email = randomemail;
     }
-    public void setcellphone(){
-        String randomcellphone="09";       
-        for(int i=0;i<8;i++){
-            randomcellphone+=(char)(Math.random()*(MAXCHAR_NUMBER-MINCHAR_NUMBER+1)+MINCHAR_NUMBER);
+
+    public void setcellphone() {
+        String randomcellphone = "09";
+        for (int i = 0; i < 8; i++) {
+            randomcellphone += (char) (Math.random() * (MAXCHAR_NUMBER - MINCHAR_NUMBER + 1) + MINCHAR_NUMBER);
         }
-        this.cellphone=randomcellphone;
+        this.cellphone = randomcellphone;
     }
-    public void setgrade(){
-        studentgrade=new ArrayList();
-        Grade grade=new Grade();
+
+    public void setgrade() {
+        studentgrade = new ArrayList();
+        Grade grade = new Grade();
         studentgrade.add(grade);
     }
-    public int getid(){
+
+    public int getid() {
         return id;
     }
-    public String getname(){
+
+    public String getname() {
         return name;
     }
-    public String getmail(){
+
+    public String getmail() {
         return email;
     }
-    public String getcellphone(){
+
+    public String getcellphone() {
         return cellphone;
     }
-    public ArrayList<Grade> getgrade(){
+
+    public ArrayList<Grade> getgrade() {
         return studentgrade;
-    }    
+    }
+
     @Override
-    public String toString(){
-        String studentallgrade="";
-        for(Grade i:studentgrade){
-            studentallgrade+=i.toString();
+    public String toString() {
+        String studentallgrade = "";
+        for (Grade i : studentgrade) {
+            studentallgrade += i.toString();
         }
-        return "學生編號:"+this.id+"  學生姓名:"+this.name+"  信箱:"+this.email
-                +"  手機號碼:"+this.cellphone+"  成績單:"+studentallgrade;
+        return "學生編號:" + this.id + "  學生姓名:" + this.name + "  信箱:" + this.email
+                + "  手機號碼:" + this.cellphone + "  成績單:" + studentallgrade;
     }
 }
